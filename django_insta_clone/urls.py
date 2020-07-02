bcb0d94059be8298dcf3bfc84e3b61edb8c58861
Youngjae Kang <kyj8028@hotmail.com>
2020-07-02 12:48:21 +0900

    bootstrap template -ing

--- django_insta_clone/urls.py
+++ django_insta_clone/urls.py
@@ -2,11 +2,13 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
-
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='root.html'), name='root')
 ]
+# re_path를 쓰면 정규 표현식이기 때문에 '' 같은 빈 주소는 아무 주소나 매칭이 된다.
 
 if settings.DEBUG:
     import debug_toolbar
