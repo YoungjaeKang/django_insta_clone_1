3873120459dd16acdc095556a5f40059efd90f54
Youngjae Kang <kyj8028@hotmail.com>
2020-07-07 22:59:46 +0900

    signup layout complete

--- accounts/views.py
+++ accounts/views.py
@@ -7,9 +7,10 @@ def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
         if form.is_valid():
-            user = form.save()
+            form.save()
             messages.success(request, '회원 가입을 환영합니다.')
-            return redirect("/")
+            next_url = request.GET.get('next', '/')
+            return redirect(next_url)
     else:
         form = SignupForm()
     return render(request, 'accounts/signup_form.html', {
