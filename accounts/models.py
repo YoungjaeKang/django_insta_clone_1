d9bdb8a6b8f13bcfa94cc21f0c9b819b24179b38
Youngjae Kang <kyj8028@hotmail.com>
2020-07-31 00:14:57 +0900

    Post Detail complete

--- accounts/models.py
+++ accounts/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractUser
 from django.core.mail import send_mail
 from django.core.validators import RegexValidator
 from django.db import models
+from django.shortcuts import resolve_url
 from django.template.loader import render_to_string
 
 
@@ -24,6 +25,13 @@ class User(AbstractUser):
     @property
     def name(self):
         return f"{self.first_name} {self.last_name}"
+
+    @property
+    def avatar_url(self):
+        if self.avatar:
+            return self.avatar.url
+        else:
+            resolve_url("pydenticon_image", self.username)
 
     def send_welcome_email(self):
         subject = render_to_string("accounts/welcome_email_subject.txt", {
